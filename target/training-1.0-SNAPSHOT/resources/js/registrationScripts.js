08032fe165a6e30506196534f4a332a89ca90bd5
Mello <tiago.mello@sap.com>
2018-09-27 10:35:00 -0300

    .gitignore fixed

--- target/training-1.0-SNAPSHOT/resources/js/registrationScripts.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    var form = $("#register-form");
-    form.on('submit', function(e){
-        if($('#password').val() != $('#matchingPassword').val()){
-            e.preventDefault();
-            alert("The passwords must match!")
-        }
-    });
-})();
