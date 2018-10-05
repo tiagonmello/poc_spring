901fcb9008d58ac9a403fd397dbe0ac802f8bc5d
Mello <tiago.mello@sap.com>
2018-10-05 10:38:45 -0300

    Removed unused imports and blank lines

--- src/main/java/com/sap/controllers/MemberController.java
+++ src/main/java/com/sap/controllers/MemberController.java
@@ -3,7 +3,6 @@ package com.sap.controllers;
 import com.sap.MyUserPrincipal;
 import com.sap.models.Event;
 import com.sap.dtos.EventDto;
-import com.sap.models.SpecialDay;
 import com.sap.models.User;
 import com.sap.service.EventService;
 import com.sap.service.SpecialDayService;
@@ -35,9 +34,7 @@ public class MemberController {
         model.addAttribute("user",loggedUser);
         model.addAttribute("event",new Event());
         model.addAttribute("calendarList",teamCalendarService.getTeamCalendarList(loggedUser.getTeam()));
-
         model.addAttribute("specialDayList",specialDayService.getSpecialDayList(loggedUser.getTeam()));
-
         model.addAttribute("dateList",teamCalendarService.getDateList(loggedUser.getTeam()));
         model.addAttribute("eventList",eventService.getEventsByUser(loggedUser.getUserName()));
         return "memberPage";
