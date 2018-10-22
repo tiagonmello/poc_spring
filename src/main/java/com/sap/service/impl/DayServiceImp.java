a9637ca8770096c035d9bd60d4d7729f4e31ea66
Mello <tiago.mello@sap.com>
2018-10-22 13:57:01 -0300

    Updating a single day limits will delete all registered events of that day and auto assign them again

--- src/main/java/com/sap/service/impl/DayServiceImp.java
+++ src/main/java/com/sap/service/impl/DayServiceImp.java
@@ -1,8 +1,13 @@
 package com.sap.service.impl;
 
 import com.sap.Dao.DayDao;
+import com.sap.Dao.EventDao;
 import com.sap.models.Day;
+import com.sap.models.Event;
+import com.sap.models.User;
 import com.sap.service.DayService;
+import com.sap.service.EventService;
+import com.sap.service.UserService;
 
 import javax.annotation.Resource;
 
@@ -10,6 +15,18 @@ public class DayServiceImp implements DayService {
 
     @Resource
     private DayDao dayDao;
+
+    @Resource
+    private EventDao eventDao;
+
+    @Resource
+    private EventDao eventDeleteDao;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private EventService eventService;
 
     @Override
     public Day getDayById(Integer id){
@@ -24,14 +41,25 @@ public class DayServiceImp implements DayService {
         if(day.getCalendar().getDayLimit() + day.getCalendar().getLateLimit() != dayLimit + lateLimit){
             return;
         }
-        // If it would create limit inconsistency on the day
-        if(day.getCurrentDay() > dayLimit || day.getCurrentLate() > lateLimit){
-            return;
-        }
 
+        // Updates day limits
         day.setDayLimit(dayLimit);
         day.setLateLimit(lateLimit);
+        day.setCurrentDay(0);
+        day.setCurrentLate(0);
         dayDao.updateDay(day);
-    }
 
+        // Deletes already registered events of this day
+        for(Event registeredEvent : eventDao.getEventsByDateAndTeam(day.getDayDate(),day.getCalendar().getTeam())){
+            Event deleteEvent = new Event();
+            deleteEvent.setId(registeredEvent.getId());
+            eventDeleteDao.deleteEvent(deleteEvent);
+        }
+
+        // Recreates events for the day
+        for(User user : userService.getUsersByTeam(day.getCalendar().getTeam())){
+            eventService.createDefaultEvent(user, day.getDayDate());
+        }
+
+    }
 }
